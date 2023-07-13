fe97691af15c43ffc804d7497f6c3b250baee073
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-13 11:48:48 +0530

    [Prasanna] Add: Structure Folder Created

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class program
+    {
+        public void Main()
+        {
+            Console.WriteLine("Welcome to the Snake And Ladder Program");
+        }
+    }
+}
